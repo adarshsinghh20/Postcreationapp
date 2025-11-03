f545291c344322a36edc0aa732549d9b1995b023
adarshsinghh20 <adarshsing.0108@gmail.com>
2025-11-04 01:16:26 +0530

    Profile pic addition and uploading done using Multer

--- app.js
+++ app.js
@@ -3,35 +3,22 @@ const app= express();
 const cookieParser = require('cookie-parser');
 const userModel = require("./models/user");
 const postModel = require("./models/post");
-const multer = require("multer");
 const crypto = require("crypto");
 const path=require("path");
+const upload = require("./config/multerconfig");
 
 
 app.set("view engine","ejs");
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname,"public")));
 const bcrypt=require('bcrypt');
 const jwt = require("jsonwebtoken");
 
 
   
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./public/images/uploads");
-  },
-  filename: function (req, file, cb) {
-    crypto.randomBytes(12 ,function(err,bytes){
-      const fn=bytes.toString("hex") + path.extname(file.originalname);
-        cb(null, fn);
-    });
-    
-  },
-});
-
-const upload = multer({ storage: storage });
 
 app.get("/", (req, res) => {
   res.render("index");
@@ -136,12 +123,17 @@ app.get("/edit/:id", isloggedin, async (req, res) => {
 });
 
 ////..............................Multer................
-app.get("/test", (req, res) => {
-  res.render("test");
+
+app.get("/profile/upload", (req, res) => {
+  res.render("profileupload");
 });
 
-app.post("/upload",upload.single("image"),(req,res) =>{
- console.log(req.file);
+app.post("/upload",isloggedin, upload.single("image"), async (req, res) => {
+  let user = await userModel.findOne({email:req.user.email});
+  user.profilepic = req.file.filename;
+  await user.save();
+  res.redirect("/profile");
 });
+
 
 app.listen(3000); 
--- models/user.js
+++ models/user.js
@@ -6,6 +6,10 @@ const userSchema=mongoose.Schema({
     age:Number,
     email:String,
     password:String,
+    profilepic:{
+        type:String,
+        default:"defaultpic.png"
+    },
     posts:[
         {type: mongoose.Schema.Types.ObjectId, ref:"post"}
     ]
